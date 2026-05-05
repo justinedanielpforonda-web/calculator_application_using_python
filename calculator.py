525a2fec7ec82ea922b74fe1e611f96dcbac7441
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-05-05 22:38:30 +0800

    user can now chooses between operations

--- calculator.py
+++ calculator.py
@@ -1,13 +1,24 @@
+print("-" * 50)
+print("calculator".center(50))
+print("-" * 50)
+
+print("Operation please")
+print(" Addtion (1)\n",
+      "Substraction(2)\n",
+      "Multiplication (3)\n",
+      "Division (4)\n")
+user_num = input("Enter here: ")
+
 num1 = float(input("Enter you 1st number please: "))
 num2 = float(input("Enter your 2nd number please: "))
 
-#addtion
-answer = num2 + num1
-#subtraction
-answer = num1 - num2
-#multiplication 
-answer = num1 * num2
-#division 
-answer = num1/num2
+if user_num == "1":
+    answer = num2 + num1
+elif user_num == "2":
+    answer = num1 - num2
+elif user_num == "3":
+    answer = num1 * num2
+elif user_num == "4":
+    answer = num1/num2
 
 print(answer)
