e81835fbd1ed0a949d850db0d9e9f41ac80a6325
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-05-05 23:27:55 +0800

    Program can now loop and continue from the start

--- calculator.py
+++ calculator.py
@@ -1,38 +1,51 @@
 def clear():
     import os
     os.system('cls' if os.name == 'nt' else 'clear')
+while True:
+    print("-" * 50)
+    print("calculator".center(50))
+    print("-" * 50)
 
-print("-" * 50)
-print("calculator".center(50))
-print("-" * 50)
+    print("Operation please")
+    print(" Addtion        (1)\n",
+        "Substraction   (2)\n",
+        "Multiplication (3)\n",
+        "Division       (4)\n",
+        "Exit           (5)\n")
+    
+    user_num = input("Enter here: ")
+    if user_num == "5":
+            print("Goodbye!")
+            break
 
-print("Operation please")
-print(" Addtion        (1)\n",
-      "Substraction   (2)\n",
-      "Multiplication (3)\n",
-      "Division       (4)\n")
-user_num = input("Enter here: ")
-
-clear()
-
-num1 = float(input("Enter you 1st number please: "))
-num2 = float(input("Enter your 2nd number please: "))
-
-
-if user_num == "1":
-    answer = num2 + num1
-elif user_num == "2":
-    answer = num1 - num2
-elif user_num == "3":
-    answer = num1 * num2
-elif user_num == "4":
-    answer = None
+    clear()
     try:
-        answer = num1 / num2
-    except ZeroDivisionError:
-        print("Denominator cannot be Zero, Please try again")
+        num1 = float(input("Enter you 1st number please: "))
+        num2 = float(input("Enter your 2nd number please: "))
+    except ValueError:
+        print("Please Enter number that 1-5")
+        break
 
-    if answer is not None:
-        print(answer)
+    answer = None
 
-clear()
+    if user_num == "1":
+        answer = num2 + num1
+    elif user_num == "2":
+        answer = num1 - num2
+    elif user_num == "3":
+        answer = num1 * num2
+    elif user_num == "4":
+        answer = None
+        try:
+            answer = num1 / num2
+        except ZeroDivisionError:
+            print("Denominator cannot be Zero, Please try again")
+            continue
+    else:
+        print("invalid operation\n")
+        continue
+    
+    print("Answer: ",answer)
+    input("Press enter to Continue")
+
+    clear()
