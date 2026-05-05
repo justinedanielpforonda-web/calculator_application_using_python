5e739d30e06713ceabb9523ba6622293f726b954
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-05-05 22:23:07 +0800

    basic operations

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter you 1st number please: "))
+num2 = float(input("Enter your 2nd number please: "))
+
+#addtion
+answer = num2 + num1
+#subtraction
+answer = num1 - num2
+#multiplication 
+answer = num1 * num2
+#division 
+answer = num1/num2
+
+print(answer)
